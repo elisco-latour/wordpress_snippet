8a3f77da6392131143c21e3d2481d77cb1900b2e
Eli <eliscolatour@gmail.com>
2020-06-17 23:05:22 +0000

    Update get the post tag dynamically

--- wp_query_example.php
+++ wp_query_example.php
@@ -1,10 +1,19 @@
 <?php
 /**
-Selectionner des posts bases sur un tag specifique 
-Exemple ici" peche
+Selectionner des articles relatifs a un autre en via l'etiquette de ce poste
 */
+
+// Recuperer l'ID de l'article courant
+$current_post_ID = get_the_ID();
+
+// Recuperer l'etiauette liee a cet article
+// get_the_tags() retourne normalement un tableau de tous les etiquettes du poste courant
+// Ici nous recuperons juste le premier element de ce tableau en se basant sur le fait 
+// Qu'il n'y a qu'une seule etiquette par article
+$current_post_tag = get_the_tags($current_post_ID)[0];
+
 // Specifier les arguments de la requete
-$args = array('tag' => 'peche',);
+$args = array('tag' => $current_post_tag,);
 
 // Il est possible que le tag soit référencé par son id
 // par exemple avec une etiquette dont le id = 15
